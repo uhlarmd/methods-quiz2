1f287d2f7f99318eb43d34280dae9b6de0c1724c
Matt Uhlar <mattuhlar@gmail.com>
2015-09-11 10:23:07 -0600

    test squirrels_play complete

--- methods_quiz2.rb
+++ methods_quiz2.rb
@@ -1,5 +1,48 @@
-module MethodsQuiz
-	
-	
+module MethodsQuiz2
+	def without_doubles?(a,b)
+		if a == b
+		 if a == 6
+		 	a = 1
+		else
+			a = a + 1
+			end
+		end
+		return a + b
+	end
 
+	def max_maybe(a,b)
+
+		if a==b
+			return 0
+		elsif a % 5 == b % 5
+			if a < b
+				return a
+			else
+				return b
+		end
+			if a < b
+			return b
+		else
+			return a
+			end
+		end
+	end
+
+	def squirrels_play?
+		if b == true
+			if a == 100 && a >= 60
+				return true
+			else
+				return false
+			end
+		elsif b==false
+			if a<=90 && a>=60
+				return true
+			else
+				return false
+			end
+		end
+			end
+			
+	end
 end
--- methods_quiz2_test.rb
+++ methods_quiz2_test.rb
@@ -12,5 +12,23 @@ class MethodsQuizTest < MiniTest::Test
 	end
 
 	# TODO - write tests here
+	def test_without_doubles
+		assert_equal 11, @m.without_doubles?(5,5)
+		assert_equal 11, @m.without_doubles?(6,5)
+		assert_equal 7, @m.without_doubles?(6,6)
+		assert_equal 5, @m.without_doubles?(1,4)
+	end
 
+	def test_max_maybe
+		assert_equal 7, @m.max_maybe(5,7)
+		assert_equal 3, @m.max_maybe(2,3)
+		assert_equal 0, @m.max_maybe(9,9)
+	end
+
+	def test_squirrels_play
+		assert_equal false, @m.squirrels_play?(95,false)
+		assert_equal true, @m.squirrels_play?(95,true)
+		assert_equal true, @m.squirrels_play?(60,false)
+		assert_equal true, @m.squirrels_play?(60,true)
+	end
 end
